edd35559275970ff60fb8bb6d0e5d58cb8c36497
2l8 <dyakovks@gmail.com>
2016-08-10 19:17:52 +0300

    first commit

---
+++ app/src/main/java/com/example/user/ormlite2/Database/Setting.java
@@ -0,0 +1,41 @@
+package com.example.user.ormlite2.Database;
+
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+
+@DatabaseTable(tableName = "setting")
+public class Setting {
+
+    @DatabaseField(generatedId = true)
+    private int Id;
+
+
+    @DatabaseField(canBeNull = false, dataType = DataType.STRING, unique = true)
+    private String Name;
+
+    @DatabaseField(canBeNull = false, dataType = DataType.INTEGER, defaultValue = "0")
+    private int Val;
+
+    public Setting() {
+    }
+
+    public Setting(String name, int val) {
+        Name = name;
+        Val = val;
+    }
+
+    @Override
+    public String toString() {
+        return "Settings{" +
+                "Id=" + Id +
+                ", Name='" + Name + '\'' +
+                ", Val=" + Val +
+                '}';
+    }
+
+    public int getVal() {
+        return Val;
+    }
+}
